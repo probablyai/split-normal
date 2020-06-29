7f38ab00ce73e7235804db282aee5b7629546363
Tárik S. Salem <tarik@users.noreply.github.com>
2020-06-29 16:38:26 +0200

    Support for JAX automatic differentiation (#1)
    * Changed the behaviour of the JAX implementation to mimic (to some extent) the SciPy functions of JAX.
    * Added JAX support for automatic differentiation.
    * Added a type-check for arguments of JAX functions.
    * Added tests for JAX differentiation.

--- split_normal/_base.py
+++ split_normal/_base.py
@@ -1,9 +1,33 @@
+import sys
 import numpy as np
 
 __all__ = [
+    'whoami',
+    'check_array_like',
     'map_as_float_array',
     'convert_negative_to_nan'
 ]
+
+from jax.numpy import ndarray, isscalar
+
+
+def whoami():
+    return sys._getframe(1).f_code.co_name
+
+
+def is_array_like(x):
+    return isinstance(x, ndarray) or isscalar(x)
+
+
+def check_array_like(*args, func_name=None):
+    arg_pos, arg_type = next(
+        ((i, type(arg)) for i, arg in enumerate(args) if not is_array_like(arg)),
+        (None, None)  # default
+    )
+    if arg_pos or arg_type:
+        msg = f"Function{f' `{func_name}()` ' if func_name else ' '}" \
+              f"requires `ndarray` or scalar arguments but got `{arg_type}` at position {arg_pos}."
+        raise TypeError(msg)
 
 
 def map_as_float_array(*args):
