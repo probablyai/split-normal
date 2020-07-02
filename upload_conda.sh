6b7b5d5311e9320ea2cf160d1311bdd1f107b596
Tárik S. Salem <tarik@salem.cz>
2020-07-02 12:03:20 +0200

    :bookmark: New release.

--- upload_conda.sh
+++ upload_conda.sh
@@ -3,6 +3,6 @@
 cd recipe/build
 
 anaconda login
-anaconda upload --user probablyai linux-64/*
-anaconda upload --user probablyai osx-64/*
-anaconda upload --user probablyai win-64/*
+anaconda upload --user probablyai linux-64/*.tar.bz2
+anaconda upload --user probablyai osx-64/*.tar.bz2
+anaconda upload --user probablyai win-64/*.tar.bz2
